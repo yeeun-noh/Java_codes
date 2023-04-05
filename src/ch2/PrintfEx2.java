8d52cf958b51eb0e0555b858240e9e59f16c1f55
yeeun-noh <gkdms8517@naver.com>
2023-04-05 12:45:31 +0900

    second

--- src/ch2/PrintfEx2.java
+++ src/ch2/PrintfEx2.java
@@ -10,6 +10,18 @@ public class PrintfEx2 {
 		float f3 = 3.14e3f;
 		double d = 1.23456789;
 		
+		System.out.printf("f1 = %f, %e, %g%n", f1, f1, f1);
+		System.out.printf("f2 = %f, %e, %g%n", f2, f2, f2);
+		System.out.printf("f3 = %f, %e, %g%n", f3, f3, f3);
+		
+		System.out.printf("d = %f%n", d); // 마지막 자리 반올림됨
+		System.out.printf("d = %14.10f%n", d); // 전체 14자리 중 소수점 10자리 (%전체자리.소수점아래자리f)
+		
+		System.out.printf("[12345678901234567890]%n", d);
+		System.out.printf("[%s]%n", url);
+		System.out.printf("[%20s]%n", url); // 오른쪽 정렬
+		System.out.printf("[%-20s]%n", url); // 왼쪽 정렬
+		System.out.printf("[%.8s]%n", url); // 왼쪽에서 8글자만	
 	}
 
 }
--- src/ch2/ScannerEx.java
+++ src/ch2/ScannerEx.java
@@ -1,6 +1,7 @@
 package ch2;
 
-import java.util.Scanner;
+// import java.util.*; 
+import java.util.Scanner; // Scanner를 사용하기 위해 추가
 
 public class ScannerEx {
 
@@ -8,12 +9,12 @@ public class ScannerEx {
 		
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("문자열 입력 하세요 : ");
-		String input = sc.nextLine(); // 입력받은 내용을 input에 저장
+		System.out.println("두자리 정수를 하나 입력해주세요 : ");
+		String input = sc.nextLine(); // 입력받은 내용을 input에 저장 
 		int num = Integer.parseInt(input); // 입력받은 문자열을 숫자로 변환
 		
 		System.out.println("입력내용 : " + input);
-		System.out.printf("num=%d%n", num);
+		System.out.printf("num= %d%n", num);
 	}
 
 }
