360fb97ec245c35f322c6150509a424fea6c463a
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-06 10:47:08 +0900

    Update ScannerEx.java

--- src/ch2/ScannerEx.java
+++ src/ch2/ScannerEx.java
@@ -12,6 +12,7 @@ public class ScannerEx {
 		System.out.println("두자리 정수를 하나 입력해주세요 : ");
 		String input = sc.nextLine(); // 입력받은 내용을 input에 저장 
 		int num = Integer.parseInt(input); // 입력받은 문자열을 숫자로 변환
+		// (만일 숫자가 아닌 문자 또는 기호를 입력하면, 입력받은 문자열을 숫자로 변환하는 과정 'Integer.parseInt()' 에서 에러가 발생)
 		
 		System.out.println("입력내용 : " + input);
 		System.out.printf("num= %d%n", num);
