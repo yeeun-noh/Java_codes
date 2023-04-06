509d48b424a7e5168c36943884eba04cd9a98a16
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-06 12:25:46 +0900

    Create OperatorEx6.java

---
+++ src/ch3/OperatorEx6.java
@@ -0,0 +1,14 @@
+package ch3;
+
+public class OperatorEx6 {
+
+	public static void main(String[] args) {
+		
+		byte a = 10;
+		byte b = 20;
+//	byte c = a + b;  => 에러; 형전환 필요, (a+b)의 값이 int형이기 때문
+		byte c = (byte)(a + b);
+		System.out.println(c);
+	}
+
+}
