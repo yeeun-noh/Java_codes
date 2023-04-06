b482c7b1b341e2b4edf1ea711e26b9f680d6ce2b
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-06 09:30:34 +0900

    Update OperatorEx4.java

--- src/ch3/OperatorEx4.java
+++ src/ch3/OperatorEx4.java
@@ -6,11 +6,11 @@ public class OperatorEx4 {
 
 		int i = -10;
 		i = +i;
-		System.out.println(i); // -10
+		System.out.println(i); // -10=> -10
 		
 		i = -10;
 		i = -i;
-		System.out.println(i); // 10
+		System.out.println(i); // -10=> 10
 		// 부호연산자'-'는 피연산자의 부호를 반대로 변경한 결과를 반환 (양수->음수, 음수->양수)
 		// 부호연산자'+'는 쓰이는경우 거의없음
 	}
