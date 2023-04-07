f3d032c0bc5d1987f31aa3922bbc787583ca23d6
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-07 11:34:00 +0900

    Create OperatorExx15.java

---
+++ src/ch3/OperatorExx15.java
@@ -0,0 +1,12 @@
+package ch3;
+
+public class OperatorExx15 {
+
+	public static void main(String[] args) {
+
+		char lowerCase = 'a';
+		char upperCase = (char)(lowerCase - 32); //'A'
+		System.out.println(upperCase);
+	}
+
+}
