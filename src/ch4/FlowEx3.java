28debbde40acb3b2be0f6ccf35314f25c1d89ff6
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-07 10:30:50 +0900

    Create FlowEx3.java

---
+++ src/ch4/FlowEx3.java
@@ -0,0 +1,21 @@
+package ch4;
+
+import java.util.Scanner;
+
+public class FlowEx3 {
+
+	public static void main(String[] args) {
+
+		System.out.print("숫자를 하나 입력하세요 >>");
+
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		
+		if(input == 0) {
+			System.out.println("입력한 숫자: 0");
+		} else {
+			System.out.println("입력한 숫자: 0아님");
+		}
+	}
+
+}
