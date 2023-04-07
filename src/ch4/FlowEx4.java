aaa2e031d3c1a7bc0063c9bc6c9a8eb1df3c698e
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-07 10:34:32 +0900

    Create FlowEx4.java

---
+++ src/ch4/FlowEx4.java
@@ -0,0 +1,29 @@
+package ch4;
+
+import java.util.Scanner;
+
+public class FlowEx4 {
+
+	public static void main(String[] args) {
+
+		int score = 0;
+		char grade = ' '; // 공백으로 초기화
+
+		System.out.print("점수를 입력하세요 >>");
+		Scanner sc = new Scanner(System.in);
+		score = sc.nextInt();
+		
+		if(score >= 90) {
+			grade = 'A';
+		} else if(score >= 80) {
+			grade = 'B';
+		} else if(score >= 70) {
+			grade = 'C';
+		}else {
+			grade = 'D';
+		}
+		
+		System.out.println("당신의 점수는 " + grade);
+	}
+
+}
