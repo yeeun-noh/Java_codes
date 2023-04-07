90303b3747c630aaae9dc15362f6cc43d61c4130
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-07 11:58:46 +0900

    Update OperatorExx16.java

--- src/ch3/OperatorExx16.java
+++ src/ch3/OperatorExx16.java
@@ -9,6 +9,7 @@ public class OperatorExx16 {
 //		(int)(pi * 1000) / 1000f;
 //		-> (int)(3141.592f) / 1000f;
 //		-> 3141 / 1000f;
+//		-> 3141.0f / 1000f;
 //		-> 3.141f
 
 		System.out.println(shortPi); // 3.141
