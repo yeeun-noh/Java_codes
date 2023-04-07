c12feccb1795658a5582787af1b062e8bb87fde4
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-07 11:31:29 +0900

    Create OperatorExx13.java

---
+++ src/ch3/OperatorExx13.java
@@ -0,0 +1,15 @@
+package ch3;
+
+public class OperatorExx13 {
+
+	public static void main(String[] args) {
+
+		char c1 = 'a';
+
+//		char c2 = c1 + 1;	//에러
+		char c2 = 'a' + 1;	//'a' 다음인 'b'가 출력
+
+		System.out.println(c2);
+	}
+
+}
