9809e66fab870a4e6a7f95e4c3455ef09a538a56
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-07 12:12:03 +0900

    Create OperatorExx19.java

---
+++ src/ch3/OperatorExx19.java
@@ -0,0 +1,13 @@
+package ch3;
+
+public class OperatorExx19 {
+
+	public static void main(String[] args) {
+
+		int x = 10, y = 8;
+		
+		System.out.printf("%d를 %d로 나누면,%n", x, y);
+		System.out.printf("몫: %d, 나머지: %d%n", x/y, x%y);
+	}
+
+}
