0d0d720be60758657477090c9738e03293d211c8
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-08 22:27:12 +0900

    Update OperatorExx23.java

--- src/ch3/OperatorExx23.java
+++ src/ch3/OperatorExx23.java
@@ -13,15 +13,15 @@ public class OperatorExx23 {
 		System.out.printf("str1.equals(\"abc\")  ?  %b%n", str1.equals("abc"));
 		System.out.printf("str2.equals(\"abc\")  ?  %b%n", str2.equals("abc"));
 		System.out.printf("str2.equalsIgnoreCase(\"ABC\")  ?  %b%n", str2.equalsIgnoreCase("ABC"));
-		// equalsIgnoreCase() : 대소문자 구별X
-	
-//		(실행결과 :)
-//		"abc"== "abc"  ?  true
-//		 str1== "abc"  ?  true
-//		 str2== "abc"  ?  false
-//		str1.equals("abc")  ?  true
-//		str2.equals("abc")  ?  false
-//		str2.equalsIgnoreCase("ABC")  ?  true
+		// equalsIgnoreCase() : 대소문자 구별X	
 	}
 
 }
+
+//	(실행결과 :)
+//	"abc"== "abc"  ?  true
+//	 str1== "abc"  ?  true
+//	 str2== "abc"  ?  false
+//	str1.equals("abc")  ?  true
+//	str2.equals("abc")  ?  false
+//	str2.equalsIgnoreCase("ABC")  ?  true
