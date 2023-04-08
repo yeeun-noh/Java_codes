e2d566fcaa4fe48905f67f89bd4f28f6564a37aa
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-08 20:15:44 +0900

    Update OperatorExx24.java

--- src/ch3/OperatorExx24.java
+++ src/ch3/OperatorExx24.java
@@ -26,6 +26,16 @@ public class OperatorExx24 {
 		System.out.printf("ch= '%c', 'A' <= ch && ch <= 'Z' = %b%n", ch, 'A' <= ch && ch <= 'Z');
 		
 		ch = 'q';
-		System.out.printf("ch= '%c', ch == 'q' || ch == 'Q' = %b%n", ch, ch == 'q' || ch == 'Q');	
+		System.out.printf("ch= '%c', ch == 'q' || ch == 'Q' = %b%n", ch, ch == 'q' || ch == 'Q');
+
+//		(실행결과 :)
+//			
+//			x= 15, 10 < x && x < 20 = true
+//			x=  6, x % 2 == 0 || x % 3 == 0 && x % 6 != 0 = true
+//			x=  6, (x % 2 == 0 || x % 3 == 0) && x % 6 != 0 = false
+//			ch= '1', '0' <= ch && ch <= '9' = true
+//			ch= 'a', 'a' <= ch && ch <= 'z' = true
+//			ch= 'A', 'A' <= ch && ch <= 'Z' = true
+//			ch= 'q', ch == 'q' || ch == 'Q' = true
 	}
 }
