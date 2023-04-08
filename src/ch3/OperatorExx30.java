c50b52d18468eed8e6d369258d079d6055ecb550
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-08 21:44:09 +0900

    Create OperatorExx30.java

---
+++ src/ch3/OperatorExx30.java
@@ -0,0 +1,55 @@
+package ch3;
+
+public class OperatorExx30 {
+
+	// 10진 정수를 2진수로 변환하는 메서드
+	static String toBinaryString(int x) {
+		String zero = "00000000000000000000000000000000";
+		String tmp = zero + Integer.toBinaryString(x); // Integer.toBinaryString(int i): 정수(10진수)를 2진 문자열로 변환
+		return tmp.substring(tmp.length()-32); // substring(int x, int y) : 문자열을 잘라낼때 사용 (String 에서 x부터  y번째 전까지만 출력, 나머지는 잘라냄)
+	}
+	
+	public static void main(String[] args) {
+		int dec = 8;
+		
+		System.out.printf("%d >> %d = %4d \t%s%n", dec, 0, dec >> 0, toBinaryString(dec >> 0));
+		System.out.printf("%d >> %d = %4d \t%s%n", dec, 1, dec >> 1, toBinaryString(dec >> 1));
+		System.out.printf("%d >> %d = %4d \t%s%n", dec, 2, dec >> 2, toBinaryString(dec >> 2));
+		System.out.printf("%d << %d = %4d \t%s%n", dec, 0, dec << 0, toBinaryString(dec << 0));
+		System.out.printf("%d << %d = %4d \t%s%n", dec, 1, dec << 1, toBinaryString(dec << 1));
+		System.out.printf("%d << %d = %4d \t%s%n", dec, 2, dec << 2, toBinaryString(dec << 2));
+		System.out.println();
+		
+		dec = -8;
+		System.out.printf("%d >> %d = %4d \t%s%n", dec, 0, dec >> 0, toBinaryString(dec >> 0));
+		System.out.printf("%d >> %d = %4d \t%s%n", dec, 1, dec >> 1, toBinaryString(dec >> 1));
+		System.out.printf("%d >> %d = %4d \t%s%n", dec, 2, dec >> 2, toBinaryString(dec >> 2));
+		System.out.printf("%d << %d = %4d \t%s%n", dec, 0, dec << 0, toBinaryString(dec << 0));
+		System.out.printf("%d << %d = %4d \t%s%n", dec, 1, dec << 1, toBinaryString(dec << 1));
+		System.out.printf("%d << %d = %4d \t%s%n", dec, 2, dec << 2, toBinaryString(dec << 2));
+		System.out.println();
+
+		dec = 8;
+		System.out.printf("%d >> %2d = %4d \t%s%n", dec, 0, dec >> 0, toBinaryString(dec >> 0));
+		System.out.printf("%d >> %2d = %4d \t%s%n", dec, 32, dec >> 32, toBinaryString(dec >> 32));
+	} // main의 끝
+  
+//	(실행결과 :)
+//	
+//	8 >> 0 =    8 	00000000000000000000000000001000
+//	8 >> 1 =    4 	00000000000000000000000000000100
+//	8 >> 2 =    2 	00000000000000000000000000000010
+//	8 << 0 =    8 	00000000000000000000000000001000
+//	8 << 1 =   16 	00000000000000000000000000010000
+//	8 << 2 =   32 	00000000000000000000000000100000
+//
+//	-8 >> 0 =   -8 	11111111111111111111111111111000
+//	-8 >> 1 =   -4 	11111111111111111111111111111100
+//	-8 >> 2 =   -2 	11111111111111111111111111111110
+//	-8 << 0 =   -8 	11111111111111111111111111111000
+//	-8 << 1 =  -16 	11111111111111111111111111110000
+//	-8 << 2 =  -32 	11111111111111111111111111100000
+//
+//	8 >>  0 =    8 	00000000000000000000000000001000
+//	8 >> 32 =    8 	00000000000000000000000000001000
+}
