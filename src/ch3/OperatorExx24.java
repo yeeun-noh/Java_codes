1824ead7ebf12cf1e02fb8ba8cc4f26b41e84042
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-08 22:28:48 +0900

    Update OperatorExx24.java

--- src/ch3/OperatorExx24.java
+++ src/ch3/OperatorExx24.java
@@ -27,15 +27,16 @@ public class OperatorExx24 {
 		
 		ch = 'q';
 		System.out.printf("ch= '%c', ch == 'q' || ch == 'Q' = %b%n", ch, ch == 'q' || ch == 'Q');
-
-//		(실행결과 :)
-//			
-//		x= 15, 10 < x && x < 20 = true
-//		x=  6, x % 2 == 0 || x % 3 == 0 && x % 6 != 0 = true
-//		x=  6, (x % 2 == 0 || x % 3 == 0) && x % 6 != 0 = false
-//		ch= '1', '0' <= ch && ch <= '9' = true
-//		ch= 'a', 'a' <= ch && ch <= 'z' = true
-//		ch= 'A', 'A' <= ch && ch <= 'Z' = true
-//		ch= 'q', ch == 'q' || ch == 'Q' = true
 	}
 }
+
+//	(실행결과 :)
+//		
+//	x= 15, 10 < x && x < 20 = true
+//	x=  6, x % 2 == 0 || x % 3 == 0 && x % 6 != 0 = true
+//	x=  6, (x % 2 == 0 || x % 3 == 0) && x % 6 != 0 = false
+//	ch= '1', '0' <= ch && ch <= '9' = true
+//	ch= 'a', 'a' <= ch && ch <= 'z' = true
+//	ch= 'A', 'A' <= ch && ch <= 'Z' = true
+//	ch= 'q', ch == 'q' || ch == 'Q' = true
+
