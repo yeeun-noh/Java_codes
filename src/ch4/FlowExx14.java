0555e183b17a6ba52583bc2bd2bade06097db759
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-10 11:47:07 +0900

    Create FlowExx14.java

---
+++ src/ch4/FlowExx14.java
@@ -0,0 +1,12 @@
+package ch4;
+
+public class FlowExx14 {
+
+	public static void main(String[] args) {
+
+		for(int i=1,j=10; i<=10; i++,j--) {
+			System.out.printf("%d \t %d%n", i, j); // i는 1부터 10까지 1씩 증가(i++) + J는 10부터 1까지 1씩 감소(j--)
+		}
+	}
+
+}
