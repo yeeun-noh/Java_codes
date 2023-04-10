f8d410057a80e12628d8e25e57ef992417833b73
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-10 11:34:24 +0900

    Create FlowExx12.java

---
+++ src/ch4/FlowExx12.java
@@ -0,0 +1,16 @@
+package ch4;
+
+public class FlowExx12 {
+
+	public static void main(String[] args) {
+
+		for(int i=1; i<=5; i++) 
+				System.out.println(i); // i의 값을 출력
+
+		for(int i=1; i<=5; i++) 
+			System.out.print(i); // print()를 쓰면 가로로 출력
+		
+		System.out.println();
+	}
+
+}
