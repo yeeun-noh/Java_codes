43f42ee6bd4b52fce48452533d3989ace32900c8
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-11 22:57:59 +0900

    Create FlowExx7.java

---
+++ src/ch4/FlowExx7.java
@@ -0,0 +1,23 @@
+package ch4;
+
+import java.util.Scanner;
+
+public class FlowExx7 {
+
+	public static void main(String[] args) {
+
+		int num = 0;
+		System.out.print("*을 출력할 라인의 수를 입력>");		
+		Scanner sc = new Scanner(System.in);
+		String tmp = sc.nextLine();
+		num = Integer.parseInt(tmp);
+		
+		for(int i=0; i<num; i++) {
+			for(int j=0; j<i+1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
