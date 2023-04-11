78159254d0bac9c0624b880a93f49063ac3e71e9
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-12 00:24:23 +0900

    Create FlowExx23.java

---
+++ src/ch4/FlowExx23.java
@@ -0,0 +1,28 @@
+package ch4;
+
+public class FlowExx23 {
+
+	public static void main(String[] args) {
+		
+		int i = 0;
+		
+		while(i<5) {
+			System.out.print(i + ", ");
+			i++;
+		}
+		
+		System.out.println("\n--------------");
+		
+		i = 5;
+		while(i-- != 0) {
+			System.out.print(i + ", ");
+		}
+	}
+
+}
+
+//	(실행결과 :)
+//
+//	0, 1, 2, 3, 4, 
+//	--------------
+//	4, 3, 2, 1, 0, 
