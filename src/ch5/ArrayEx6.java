d3abf58b29033165e30ca1dbf340d87405a40981
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-12 22:09:42 +0900

    Update ArrayEx6.java

--- src/ch5/ArrayEx6.java
+++ src/ch5/ArrayEx6.java
@@ -10,7 +10,7 @@ public class ArrayEx6 {
 		int max = score[0]; // 배열의 첫번째 값으로 최대값을 초기화
 		int min = score[0]; // 배열의 첫번째 값으로 최소값을 초기화
 		
-		for(int i=0; i<score.length; i++) {
+		for(int i=1; i<score.length; i++) { // 배열의 두번째 요소부터 읽기 위해서 변수i의 값을 1로 초기화(score[0] 과 score[1]부터 비교시작~)
 			if(score[i]> max) max = score[i];
 			else if(score[i] < min) min = score[i];
 		}
