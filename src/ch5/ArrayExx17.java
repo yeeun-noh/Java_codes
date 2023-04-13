7f7a579e9afd91475e0e42523413f284b1274c2b
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-13 23:40:20 +0900

    Create ArrayExx17.java

---
+++ src/ch5/ArrayExx17.java
@@ -0,0 +1,40 @@
+package ch5;
+
+public class ArrayExx17 {
+
+	public static void main(String[] args) {
+		if(args.length !=3) {
+			System.out.println("usage: java ArrayEx17 NUM OP NUM2");
+			System.exit(0); // 프로그램 종료
+		}
+		
+		int num1 = Integer.parseInt(args[0]); // 문자열을 숫자로 변환
+		char op = args[1].charAt(0); // 문자열을 문자(char)로 변환
+		int num2 = Integer.parseInt(args[2]);
+		int result = 0;
+		
+		switch(op) {
+			case'+' :
+				result = num1 + num2;
+				break;
+			case'-' :
+				result = num1 - num2;
+				break;
+			case'x' :
+				result = num1 * num2;
+				break;
+			case'/' :
+				result = num1 / num2;
+				break;
+			default :
+				System.out.println("지원하지 않는 연산!");
+		}
+		System.out.println("결과: " + result);
+		
+	}
+
+}
+
+// arguments에 아무것도 안치면 (실행결과)==> usage: java ArrayEx17 NUM OP NUM2
+// 10 + 3 치면 (실행결과)==> 결과: 13
+// 10 x 3 치면 (실행결과)==> 결과: 30
