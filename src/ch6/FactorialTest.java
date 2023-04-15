97545d6715ebc1d693c4de586e95a2ce172a71fe
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-16 02:03:26 +0900

    Create FactorialTest.java

---
+++ src/ch6/FactorialTest.java
@@ -0,0 +1,28 @@
+package ch6;
+
+// 대표적인 재귀호출의 예시: 팩토리얼(factorial) 구하기
+// f(n) = n * f(n-1), 딘 f(1) = 1
+public class FactorialTest {
+
+	public static void main(String[] args) {
+		int result = factorial(4);
+		
+		System.out.println(result);
+	}
+	
+	static int factorial(int n) {
+		int result = 0;
+		
+		if(n==1) result = 1;
+		else result = n * factorial(n-1); //다시 메서드 자신을 호출
+		
+		return result;
+		
+	}
+}
+
+/* (실행결과:)
+
+24
+
+*/
