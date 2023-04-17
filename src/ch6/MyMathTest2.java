e55efb0e16a769636c7f108f57e90fb0649abc0d
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-17 12:09:03 +0900

    Update MyMathTest2.java

--- src/ch6/MyMathTest2.java
+++ src/ch6/MyMathTest2.java
@@ -1,6 +1,25 @@
 package ch6;
 
 // 클래스메서드와 인스턴스메서드 차이
+
+class MyMath2 {
+		long a, b;
+		
+		//인스턴스 메서드
+		//인스턴스변수 a, b만을 이용해서 작업하므로 매개변수가 필요없음:
+		long add()		 { return a + b; }; // a, b는 인스턴스 변수
+		long substract() { return a - b; };
+		long multiply()  { return a * b; };
+		double divide()  { return a / b; };
+		
+		//클래스메서드
+		//인스턴스변수와 관계없이 매개변수만으로 작업이 가능:
+		static long add(long a, long b) { return a + b; }; // a, b는 지역변수
+		static long substract(long a, long b) { return a - b; };
+		static long multiply(long a, long b) { return a * b; };
+		static double divide(double a, double b) { return a / b; };
+}
+
 public class MyMathTest2 {
 	public static void main(String[] args) {
 		
@@ -24,24 +43,6 @@ public class MyMathTest2 {
 		System.out.println(MyMath2.divide(200.0, 100.0));
 		
 	}
-}
-	
-class MyMath2 {
-		long a, b;
-		
-		//인스턴스 메서드
-		//인스턴스변수 a, b만을 이용해서 작업하므로 매개변수가 필요없음:
-		long add()		 { return a + b; }; // a, b는 인스턴스 변수
-		long substract() { return a - b; };
-		long multiply()  { return a * b; };
-		double divide()  { return a / b; };
-		
-		//클래스메서드
-		//인스턴스변수와 관계없이 매개변수만으로 작업이 가능:
-		static long add(long a, long b) { return a + b; }; // a, b는 지역변수
-		static long substract(long a, long b) { return a - b; };
-		static long multiply(long a, long b) { return a * b; };
-		static double divide(double a, double b) { return a / b; };
 }
 
 /* (실행결과:)
