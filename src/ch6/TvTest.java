81b31995b0b27716c03e5a9405942955583b1170
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-17 10:16:42 +0900

    Update TvTest.java

--- src/ch6/TvTest.java
+++ src/ch6/TvTest.java
@@ -1,12 +1,13 @@
 package ch6;
 
+//Tv 클래스 설계
 class Tv {
-	//Tv의 속성(멤버변수):
+	//멤버변수, 필드 (Tv의 속성):
 	String  colour;		//색상
 	boolean power;		//전원상태(on/off)
 	int channel;		//채널
 	
-	//Tv의 기능(메서드):
+	//메서드, 멤버함수 (Tv의 기능):
 	void power() { power = !power; }	//채널을 켜거나 끄는 기능을 하는 메서드
 	void channelUp() { ++channel; }		//채널을 높이는 기능을 하는 메서드
 	void channelDown() { --channel; }	//채널을 낮추는 기능을 하는 메서드
