e3bb5b823230d9d8deb360360c0fe0498f5581f2
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-17 12:10:48 +0900

    Update TvTest4.java

--- src/ch6/TvTest4.java
+++ src/ch6/TvTest4.java
@@ -1,5 +1,17 @@
 package ch6;
 
+class Tv {
+	//Tv의 속성(멤버변수):
+	String  colour;		//색상
+	boolean power;		//전원상태(on/off)
+	int channel;		//채널
+	
+	//Tv의 기능(메서드):
+	void power() { power = !power; }	//채널을 켜거나 끄는 기능을 하는 메서드
+	void channelUp() { ++channel; }		//채널을 높이는 기능을 하는 메서드
+	void channelDown() { --channel; }	//채널을 낮추는 기능을 하는 메서드
+}
+
 class TvTest4 {
 	public static void main(String[] args) {
 		Tv[] tvArr = new Tv[3]; //길이가 3인 Tv객체배열
@@ -16,18 +28,6 @@ class TvTest4 {
 		}
 		
 	}
-}
-
-class Tv {
-	//Tv의 속성(멤버변수):
-	String  colour;		//색상
-	boolean power;		//전원상태(on/off)
-	int channel;		//채널
-	
-	//Tv의 기능(메서드):
-	void power() { power = !power; }	//채널을 켜거나 끄는 기능을 하는 메서드
-	void channelUp() { ++channel; }		//채널을 높이는 기능을 하는 메서드
-	void channelDown() { --channel; }	//채널을 낮추는 기능을 하는 메서드
 }
 
 /* (실행결과:)
