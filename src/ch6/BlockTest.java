4ebbca6a68bdbe060b8c62d15d3ae4e4155834f0
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-18 20:53:29 +0900

    Create BlockTest.java

---
+++ src/ch6/BlockTest.java
@@ -0,0 +1,41 @@
+package ch6;
+
+public class BlockTest {
+
+	//클래스 초기화 블럭:
+	//클래스가 메모리에 처음 로딩될 때 한번만 수행됨
+	static {
+		System.out.println("static { }");
+	}
+	
+	//인스턴스 초기화 블럭: 
+	//생성자와 같이 인스턴스를 생성될 때마다 수행됨
+	{
+		System.out.println("{ }");
+	}
+	
+	public BlockTest() {
+		System.out.println("생성자");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("BlockTest bt = new BlockTest();");
+		BlockTest bt = new BlockTest();
+		
+		System.out.println("BlockTest bt2 = new BlockTest();");
+		BlockTest bt2 = new BlockTest();		
+	}
+
+}
+
+/* (실행결과:)
+
+static { }
+BlockTest bt = new BlockTest();
+{ }
+생성자
+BlockTest bt2 = new BlockTest();
+{ }
+생성자
+
+*/
