a1ee5716c5333ace676c575d1fc209228613734c
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-18 10:40:07 +0900

    Update ReferenceReturnEx.java

--- src/ch6/ReferenceReturnEx.java
+++ src/ch6/ReferenceReturnEx.java
@@ -8,12 +8,15 @@ public class ReferenceReturnEx {
 		Data d = new Data();
 		d.x = 10;
 		
-		Data d2 = copy(d);
+		Data d2 = copy(d); //d = 인자값
 		System.out.println("d.x= " + d.x);
 		System.out.println("d2.x= " + d2.x);
 	}
 	
-	static Data copy(Data d) {
+	// copy메서드는 새로운 객체를 생성한 다음에, 매개변수로 넘겨받은 객체에 저장한 값을 복사해서 반환함
+	// 반환하는 값이 Data객체의 주소이므로, 반환타입이 'Data'인 것!
+	// 이 메서드의 반환타입이 'Data'이므로, 호출결과를 저장하는 변수의 타입 역시 'Data'타입의 참조변수이어야 함
+	static Data copy(Data d) { //d = 매개변수 (인자와 매개변수의 타입은 같아야함)
 		Data tmp = new Data(); //새로운 객체 tmp를 생성
 		tmp.x = d.x; //d.x의 값을 tmp.x에 복사
 		
