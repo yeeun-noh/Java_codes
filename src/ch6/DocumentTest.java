d8de9080e8d4e456255bb75f691460c0e467324e
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-18 21:26:31 +0900

    Update DocumentTest.java

--- src/ch6/DocumentTest.java
+++ src/ch6/DocumentTest.java
@@ -4,7 +4,7 @@ class Document {
 	static int count = 0;
 	String name;	// 문서명
 
-	Document(){		// 문서 생성시 문서명을 지정하지 않았을 때
+	Document(){	// 문서 생성시 문서명을 지정하지 않았을 때
 		this("제목없음" + ++count); //this()는 다음생성자를 먼저 호출하기때문에 line11을 먼저 수행하고 이자리로 돌아와 그 다음을 수행
 	}
 	
