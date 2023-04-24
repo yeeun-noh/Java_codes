cb45332edeb748bee2578c3f1739da2c4d9e40f6
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-25 01:28:25 +0900

    Update FinalCardTest.java

--- src/ch7/FinalCardTest.java
+++ src/ch7/FinalCardTest.java
@@ -1,7 +1,7 @@
 package ch7;
 
 class Cards {
-	final int NUMBER;	  //상수지만 선언과 함께 초기화 하지 않고,
+	final int NUMBER;	//상수지만 선언과 함께 초기화 하지 않고,
 	final String KIND;	//생성자에서 단 한번만 초기화 할 수 있음!
 	static int width = 100;
 	static int height = 250;
@@ -26,7 +26,7 @@ public class FinalCardTest {
 	public static void main(String[] args) {
 		
 		Cards c = new Cards("HEART", 10);
-//	c.NUMBER = 5;   //에러, cannot assign a value to final variable NUMBER
+//		c.NUMBER = 5;   //에러, cannot assign a value to final variable NUMBER
 		System.out.println(c.KIND);
 		System.out.println(c.NUMBER);
 		System.out.println(c); //==> System.out.println(c.toString());
