4909a516470c6528c1ea9a18df86a4e35b664588
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-26 02:05:19 +0900

    Create InterfaceTest.java

---
+++ src/ch7/InterfaceTest.java
@@ -0,0 +1,29 @@
+package ch7;
+
+class A1 {
+	public void methodA(B1 b) {
+		b.methodB();
+	}
+}
+
+class B1 {
+	public void methodB() {
+		System.out.println("methodB()");
+	}
+}
+
+public class InterfaceTest {
+
+	public static void main(String[] args) {
+
+		A1 a = new A1();
+		a.methodA(new B1());
+	}
+	
+}
+
+/* (실행결과:)
+
+methodB()
+
+*/
