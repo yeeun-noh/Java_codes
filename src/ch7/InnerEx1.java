6ff472eede210fe532dd7673dc3ecd17a7fb3064
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-27 23:48:06 +0900

    Update InnerEx1.java

--- src/ch7/InnerEx1.java
+++ src/ch7/InnerEx1.java
@@ -5,7 +5,7 @@ public class InnerEx1 {
 	class InstanceInner {
 		int iv = 100;
 //	static int cv = 100;				//=>에러, 인스턴스클래스에 static변수 선언불가능
-		final static int CONST = 100;	//final static은 상수이므로 가능
+		final static int CONST = 100;		//final static은 상수이므로 가능
 	}
 	
 	static class StaticInner {
@@ -16,7 +16,7 @@ public class InnerEx1 {
 	void myMethod() {
 		class LocalInner {
 			int iv = 300;
-//			static int cv = 300;			//=>에러, 지역클래스에 static변수 선언불가능
+//			static int cv = 300;		//=>에러, 지역클래스에 static변수 선언불가능
 			final static int CONST = 300;	//final static은 상수이므로 가능
 		}
 	}
