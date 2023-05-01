bbdd2e78ff095784bd19bcf3ca28a8b5412a4fda
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-05-01 23:06:06 +0900

    Create StringEx2.java

---
+++ src/ch9/StringEx2.java
@@ -0,0 +1,16 @@
+package ch9;
+//String 클래스 - 문자열 리터럴
+
+public class StringEx2 {
+
+	public static void main(String[] args) throws Exception {
+
+		//"AAA"를 담고있는 String인스턴스가 하나 생성된후,
+		//참조변수 str1, str2, str3는 모두 이 String인스턴스를 참조하게됨!
+		String str1 = "AAA";
+		String str2 = "AAA";
+		String str3 = "AAA";
+		String str4 = "BBB";
+	}
+	
+}
