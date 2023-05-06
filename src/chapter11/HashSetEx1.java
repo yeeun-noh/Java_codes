8d5d8f82d2dceee79a183dc19b5e4a6e52dd97ec
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-05-06 19:17:55 +0900

    Create HashSetEx1.java

---
+++ src/chapter11/HashSetEx1.java
@@ -0,0 +1,24 @@
+package chapter11;
+// HashSet
+
+import java.util.*;
+
+public class HashSetEx1 {
+
+	public static void main(String[] args) {
+
+		Object[] objArr = {"1", new Integer(1), "2", "2", "3", "3", "4", "4", "4"};
+		Set set = new HashSet();
+		
+		for(int i=0; i<objArr.length; i++)
+			set.add(objArr[i]);  //HashSet에 objArr의 요소들을 저장
+		System.out.println(set); //HashSet에 저장된 요소들을 출력
+	}
+	
+}
+
+/* (실행 결과:)
+
+[1, 1, 2, 3, 4]
+
+*/
